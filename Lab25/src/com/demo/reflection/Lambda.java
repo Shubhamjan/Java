ad88355f87d56484de19aeb164b51dff069d9eae
Shubhamjan <98376198+Shubhamjan@users.noreply.github.com>
2023-11-10 01:48:27 -0400

    Add files via upload

---
+++ Lab25/src/com/demo/reflection/Lambda.java
@@ -0,0 +1,19 @@
+package com.demo.reflection;
+
+public class Lambda {
+	public static void main(String[] args)
+    {
+ 
+        Runnable myThread = () ->
+        {
+ 
+            Thread.currentThread().setName("myThread");
+            System.out.println(
+                Thread.currentThread().getName()
+                + " is running");
+        };
+ 
+        Thread run = new Thread(myThread);
+        run.start();
+    }
+}
